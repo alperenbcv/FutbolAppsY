f3a337034f4b09cd14bf217c3ff228b949503556
alperenbcv <alperenbcv@gmail.com>
2024-08-18 22:38:11 +0300

    commit

--- src/FootballApp/modules/Menu.java
+++ src/FootballApp/modules/Menu.java
@@ -16,33 +16,38 @@ public class Menu {
 			menuSelection(userInput);
 		} while (userInput != 0);
 	}
-	
+
 	private static int menuEntry() {
-		
-		int userInput=-1;
+		int userInput = -1;
 		boolean validInput = false;
-		
-		while(!validInput) {
-			if(loggedManager == null)
-			System.out.println("1-Log In");
+
+		while (!validInput) {
+			if (loggedManager == null)
+				System.out.println("1-Log In");
 			System.out.println("2-Team Menu");
 			System.out.println("3-Player Menu");
-			if(loggedManager!=null)
-			System.out.println("9-Log Out");
+			if (loggedManager != null)
+				System.out.println("9-Log Out");
 			System.out.println("0-Exit");
 			System.out.print("Selection: ");
 			try {
 				userInput = sc.nextInt();
 				sc.nextLine();
-				if (loggedManager == null && (userInput == 1 )) {
-					validInput = true;
-				} else if(loggedManager!= null && (userInput == 2 || userInput == 3 || userInput==9)){
-					validInput=true;
-				} else if(userInput == 0){
-					validInput = true;
-				}
-				else {
-					System.out.println("\nYou need to log in first!");
+
+				if (loggedManager == null) {
+					if (userInput == 1 || userInput == 0) {
+						validInput = true;
+					} else if (userInput == 2 || userInput == 3) {
+						System.out.println("\nYou need to log in first!");
+					} else {
+						System.out.println("\nPlease enter a valid value!\n");
+					}
+				} else {  // loggedManager != null
+					if (userInput == 2 || userInput == 3 || userInput == 9 || userInput == 0) {
+						validInput = true;
+					} else {
+						System.out.println("\nPlease enter a valid value!");
+					}
 				}
 			} catch (InputMismatchException e) {
 				System.out.println("\nPlease enter a numeric value!");
@@ -51,6 +56,7 @@ public class Menu {
 		}
 		return userInput;
 	}
+
 	private static void menuSelection(int userInput) {
 		switch (userInput) {
 			case 1 -> loggedManager=LogInModule.managerLogIn();
